be350fade68576787d3abea5ffe60901f5543105
robbieopenpaws <robbie@openpaws.ai>
2025-09-04 12:24:52 +0100

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,38 @@
-// auth.ts - NextAuth v5 configuration
 import NextAuth from "next-auth"
-import Facebook from "next-auth/providers/facebook"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "@/lib/prisma"
 
-const prisma = new PrismaClient()
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-Facebook({
-  clientId: process.env.FACEBOOK_APP_ID!,
-  clientSecret: process.env.FACEBOOK_APP_SECRET!,
-  authorization: {
-    params: {
-      scope: "email,public_profile"
+    {
+      id: "facebook-business",
+      name: "Facebook Business",
+      type: "oauth",
+      authorization: {
+        url: "https://www.facebook.com/v19.0/dialog/oauth",
+        params: {
+          config_id: "2124466384625774", // Your Configuration ID
+          response_type: "code",
+          override_default_response_type: "true"
+        }
+      },
+      token: "https://graph.facebook.com/v19.0/oauth/access_token",
+      userinfo: "https://graph.facebook.com/me?fields=id,name,email",
+      clientId: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          image: `https://graph.facebook.com/${profile.id}/picture?type=large`
+        }
+      }
     }
-  }
-})
   ],
   callbacks: {
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
@@ -30,32 +41,12 @@ Facebook({
       return token
     },
     async session({ session, token }) {
-      // Send properties to the client by returning a new object
-      const result = { 
+      return {
         ...session,
-        accessToken: undefined as string | undefined,
-        refreshToken: undefined as string | undefined,
-        expiresAt: undefined as number | undefined
+        accessToken: token.accessToken as string,
+        refreshToken: token.refreshToken as string,
+        expiresAt: token.expiresAt as number
       }
-      
-      if (token.accessToken) {
-        result.accessToken = token.accessToken as string
-      }
-      if (token.refreshToken) {
-        result.refreshToken = token.refreshToken as string
-      }
-      if (token.expiresAt) {
-        result.expiresAt = token.expiresAt as number
-      }
-      
-      return result
     }
-  },
-  pages: {
-    signIn: '/auth/signin',
-    error: '/auth/error',
-  },
-  session: {
-    strategy: "jwt",
   }
 })
