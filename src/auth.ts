a8eda3db26af5285e8ecd4031b87ba5fdbc481ed
robbieopenpaws <robbie@openpaws.ai>
2025-09-04 14:08:28 +0100

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 // auth.ts - NextAuth v5 configuration
 import NextAuth from "next-auth"
+import Facebook from "next-auth/providers/facebook"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -8,38 +9,19 @@ const prisma = new PrismaClient()
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    {
-      id: "facebook-business",
-      name: "Facebook Business",
-      type: "oauth",
+    Facebook({
+      clientId: process.env.FACEBOOK_APP_ID!,
+      clientSecret: process.env.FACEBOOK_APP_SECRET!,
       authorization: {
-        url: "https://www.facebook.com/v19.0/dialog/oauth",
         params: {
-          config_id: "2124466384625774",
-          response_type: "code"
-        }
-      },
-      token: "https://graph.facebook.com/v19.0/oauth/access_token",
-      userinfo: {
-        url: "https://graph.facebook.com/me",
-        params: {
-          fields: "id,name,email"
-        }
-      },
-      clientId: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: `https://graph.facebook.com/${profile.id}/picture?type=large`
+          scope: "email,public_profile"
         }
       }
-    }
+    })
   ],
   callbacks: {
     async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
@@ -48,6 +30,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return token
     },
     async session({ session, token }) {
+      // Send properties to the client by returning a new object
       const result = { 
         ...session,
         accessToken: undefined as string | undefined,
